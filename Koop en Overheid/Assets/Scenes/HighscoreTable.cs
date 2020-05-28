22ef7eff213b2891ab9969e251e58bf5c96fcc40
Poot <Spoot@hotmail.nl>
2020-05-28 21:58:20 +0200

    Dummy Data ordered and design added

--- Koop en Overheid/Assets/Scenes/HighscoreTable.cs
+++ Koop en Overheid/Assets/Scenes/HighscoreTable.cs
@@ -15,6 +15,8 @@ public class HighscoreTable : MonoBehaviour
         entryTemplate.gameObject.SetActive(false);
 
         float templateHeight = 80f;
+        int[] score = {100,88,73,55,47,39,26,22,14,8};
+        string[] name = {"KOOP", "RDW", "Gemeente Rotterdam", "Mark Rutte", "Willie", "Belastingdienst", "CJIB", "CBR", "DUO", "Gemeente Amsterdam"};
         for (int i = 0; i <10; i++){
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
@@ -34,13 +36,15 @@ public class HighscoreTable : MonoBehaviour
             
             entryTransform.Find("posText").GetComponent<Text>().text = rankString;
 
-            int score = Random.Range(0, 120);
+            // int score = Random.Range(0, 120);
 
-            entryTransform.Find("scoreText").GetComponent<Text>().text = score.ToString();
+            entryTransform.Find("scoreText").GetComponent<Text>().text = score[i].ToString();
 
-            string name = "KOOP";
+            // string name = "KOOP";
 
-            entryTransform.Find("nameText").GetComponent<Text>().text = name;          
+            entryTransform.Find("nameText").GetComponent<Text>().text = name[i];       
+
+            entryTransform.Find("background").gameObject.SetActive(rank % 2 == 0);   
 
         }
     }
