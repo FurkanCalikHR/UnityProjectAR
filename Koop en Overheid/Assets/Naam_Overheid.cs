c18654395b9cd9e8fba56f67526e3a9e89d7e0d2
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2020-05-28 19:30:24 +0200

    Instellingen
    Naam en Overheid input

---
+++ Koop en Overheid/Assets/Naam_Overheid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Naam_Overheid : MonoBehaviour
+{
+    public string NaamMed;
+    public string Overheid;
+    public GameObject inputFieldNaam;
+    public GameObject inputFieldOverheid;
+    public GameObject textDisplay;
+
+    public void Opslaan()
+    {
+        NaamMed = inputFieldNaam.GetComponent<Text>().text;
+        //textDisplay.GetComponent<Text>().text = "Welkom" + NaamMed;
+        Overheid = inputFieldOverheid.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welkom " + NaamMed + " van " + Overheid;
+    }
+}
