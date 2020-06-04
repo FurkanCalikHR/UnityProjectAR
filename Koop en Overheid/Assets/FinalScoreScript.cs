55014ee76b47a7a0634bc806e3e2f0ab985408f5
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2020-06-04 20:03:46 +0200

    Finale score toont foute vragen + antwoorden
    De finale score laat de foute beantwoorden vragen zien met het juiste antwoord van de vraag

--- Koop en Overheid/Assets/FinalScoreScript.cs
+++ Koop en Overheid/Assets/FinalScoreScript.cs
@@ -5,11 +5,19 @@ using UnityEngine.UI;
 
 public class FinalScoreScript : MonoBehaviour
 {
-    public Text latestScoreDisplayText, highScoreDisplayText;
+    public Text latestScoreDisplayText, highScoreDisplayText, AllQuestions;
 
     void Start()
     {
         latestScoreDisplayText.text = "Leeftijd Bereikt: " + PlayerPrefs.GetInt("latestage");
         highScoreDisplayText.text = "High Score: " + PlayerPrefs.GetInt("highscore");
+        if(PlayerPrefs.GetString("AllQuestions") == null)
+        {
+            AllQuestions.text = "Gefelicteerd u heeft alle vragen goed beantwoordt!";
+        }
+        else
+        {
+        AllQuestions.text = PlayerPrefs.GetString("AllQuestions");
+        }
     }
 }
