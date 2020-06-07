d111538fcd9b577a3330f261647a3161a62fd303
hamza <0963707@hr.nl>
2020-06-07 14:53:28 +0200

    Updated Higshcore Page

--- Koop en Overheid/Assets/Scenes/HighscoreTable.cs
+++ Koop en Overheid/Assets/Scenes/HighscoreTable.cs
@@ -8,16 +8,44 @@ public class HighscoreTable : MonoBehaviour
     private Transform entryContainer;
     private Transform entryTemplate;
 
+    private List<HighscoreEntry> highscoresEntries = new List<HighscoreEntry>();
+
     private void Awake() {
+        StartCoroutine(GetData());
+    }
+
+    IEnumerator GetData()
+    {
+        WWW www = new WWW("https://koopoverheid.000webhostapp.com/score.php");
+        yield return www;
+        string[] result = www.text.Split('\n');
+        for(int i = 0; i < result.Length; i++)
+        {
+            if(result[i] != null)
+            {
+                string[] information = result[i].Split('\t');
+                if(information.Length >= 2)
+                {
+                    string username = information[0];
+                    int score = int.Parse(information[1]);
+                    highscoresEntries.Add(new HighscoreEntry(username, score));
+                }
+            }
+        }
+        BuildHighscores();
+        
+    }
+
+    private void BuildHighscores()
+    {
         entryContainer = transform.Find("highscoreEntryContainer");
         entryTemplate = entryContainer.Find("highscoreEntryTemplate");
 
         entryTemplate.gameObject.SetActive(false);
 
         float templateHeight = 80f;
-        int[] score = {100,88,73,55,47,39,26,22,14,8};
-        string[] name = {"KOOP", "RDW", "Gemeente Rotterdam", "Mark Rutte", "Willie", "Belastingdienst", "CJIB", "CBR", "DUO", "Gemeente Amsterdam"};
-        for (int i = 0; i < 10; i++){
+        for (int i = 0; i < highscoresEntries.Count; i++)
+        {
             Transform entryTransform = Instantiate(entryTemplate, entryContainer);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
             entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
@@ -25,27 +53,33 @@ public class HighscoreTable : MonoBehaviour
 
             int rank = i + 1;
             string rankString;
-            switch (rank) {
+            switch (rank)
+            {
                 default:
                     rankString = rank + "TH"; break;
 
-            case 1: rankString = "1ST"; break; 
-            case 2: rankString = "2ND"; break; 
-            case 3: rankString = "3RD"; break;  
+                case 1: rankString = "1ST"; break;
+                case 2: rankString = "2ND"; break;
+                case 3: rankString = "3RD"; break;
             }
-            
+
             entryTransform.Find("posText").GetComponent<Text>().text = rankString;
-
-            // int score = Random.Range(0, 120);
-
-            entryTransform.Find("scoreText").GetComponent<Text>().text = score[i].ToString();
-
-            // string name = "KOOP";
-
-            entryTransform.Find("nameText").GetComponent<Text>().text = name[i];       
-
-            entryTransform.Find("background").gameObject.SetActive(rank % 2 == 0);   
+            entryTransform.Find("scoreText").GetComponent<Text>().text = highscoresEntries[i].score.ToString();
+            entryTransform.Find("nameText").GetComponent<Text>().text = highscoresEntries[i].username;
+            entryTransform.Find("background").gameObject.SetActive(rank % 2 == 0);
 
         }
+    }
+}
+
+public class HighscoreEntry
+{
+    public string username;
+    public int score;
+
+    public HighscoreEntry(string username, int score)
+    {
+        this.username = username;
+        this.score = score;
     }
 }
