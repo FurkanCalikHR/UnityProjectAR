cb7b96be700e235f8b36bb82f0e597e6bfb55d48
hamza <0963707@hr.nl>
2020-06-12 14:36:45 +0200

    Updated over scene
    Over scene now supports multiple pages and are all correctly scrollable

---
+++ Koop en Overheid/Assets/OverSceneScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class OverSceneScript : MonoBehaviour
+{
+    [TextArea(15, 20)]
+    public string[] overSceneText;
+
+    public TextMeshProUGUI displayedText, pageNumbers;
+
+    private int currentPageIndex;
+
+    public void Start()
+    {
+        currentPageIndex = 0;
+        pageNumbers.text = (currentPageIndex + 1) + " / " + overSceneText.Length;
+    }
+
+    public void PreviousPage()
+    {
+        currentPageIndex--;
+
+        if (currentPageIndex < 0)
+        {
+            currentPageIndex = 0;
+        }
+
+        displayedText.text = overSceneText[currentPageIndex];
+        pageNumbers.text = (currentPageIndex + 1) + " / " + overSceneText.Length;
+    }
+
+    public void NextPage()
+    {
+        currentPageIndex++;
+
+        if (currentPageIndex > overSceneText.Length - 1)
+        {
+            currentPageIndex = overSceneText.Length - 1;
+        }
+
+        displayedText.text = overSceneText[currentPageIndex];
+        pageNumbers.text = (currentPageIndex + 1) + " / " + overSceneText.Length;
+    }
+}
